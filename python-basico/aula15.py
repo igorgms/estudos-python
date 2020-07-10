3d7de7e745a3ab727fcb188532055e07c9a1130a
Igor Gomes <igorgmsf@gmail.com>
2020-07-10 16:29:28 -0300

    new file added

---
+++ python-basico/aula15.py
@@ -0,0 +1,28 @@
+"""
+Formatando valores com modificadores
+
+:s = Texto (strings)
+:d - inteiros(int)
+:f - Números de ponto flutuante(float)
+:.(NÚMERO)f - Quantidade de casas decimais (float)
+:(CARACTERE) (> ou < ou ^)(QUANTIDADE)(TIPO s, d ou f)
+
+> - ESQUERDA
+< - DIREITA
+^ - CENTRO
+"""
+
+num1 = 10
+num2 = 3
+
+divisao = num1 / num2
+
+# :.2f significa que no resultado da divisão será impresso com duas casas decimais
+print('{:.2f}'.format(divisao))
+print(f'{divisao:.2f}')  # Outra forma de fazer
+
+num = 2
+print(f'{num:0>10}')
+
+num3 = 12588
+print(f'{num3:0<10}')
--- python-basico/desafios/desafio03Hora.py
+++ python-basico/desafios/desafio03Hora.py
@@ -3,11 +3,11 @@ horario = input('Qual é o horário: ')
 try:
     horario = int(horario)
 
-    if horario >= 0 and horario <= 11:
+    if horario <= 11:
         print('Bom dia!')
-    elif horario >= 12 and horario <= 17:
+    elif horario <= 17:
         print('Boa tarde!')
-    elif horario >= 18 and horario <= 23:
+    elif horario <= 23:
         print('Boa noite!')
     else:
         print('O horário não existe!')
