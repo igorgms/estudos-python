9f2ab0f4e3787de04a10e4ffc98ccb91b7828ddb
Igor Gomes <igorgmsf@gmail.com>
2020-07-13 18:12:43 -0300

    new file added

---
+++ python-basico/aula16.py
@@ -0,0 +1,16 @@
+"""
+Manipulando Strings
+
+* String indices
+* Fatiamento de strings [inicio:fim:passo]
+* Funções built-in len, abs, type, print, etc..
+"""
+# positivos
+texto = 'Python s2'
+print(texto[7])
+
+# desta forma é possível imprimir somente uma parte das strings
+nova_string = texto[0:6:2]
+print(nova_string)
+
+print(len(texto))
